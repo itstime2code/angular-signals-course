9d2313ea74e14c792abde0f3d1d02de3af592bf4
prasadmedisetti <medisetti.prasad.cse@gmail.com>
2025-04-21 11:39:51 +0530

    Added Course Card Lists.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import {MatDialog} from "@angular/material/dialog";
 import {MessagesService} from "../messages/messages.service";
 import {catchError, from, throwError} from "rxjs";
 import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 
 @Component({
     selector: 'home',
@@ -19,5 +20,40 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+    #courses = signal<Course[]>([]);
+    beginnerCourses = computed(() => {
+        const courses = this.#courses();
+        return courses.filter(
+            courses => courses.category === "BEGINNER"
+        );
+    })
+    advancedCourses = computed(() => {
+        const courses = this.#courses();
+        return courses.filter(
+            courses => courses.category === "ADVANCED"
+        );
+    })
 
+    coursesService = inject(CoursesService);
+
+    constructor() {
+        effect(() => {
+            console.log('Beginner courses: ', this.beginnerCourses());
+            console.log('Advanced courses: ', this.advancedCourses());
+        })
+
+        this.loadAllCourses().then(() => {
+            console.log('All courses loaded: ', this.#courses());
+        });
+    }
+
+    async loadAllCourses() {
+        try {
+            const coursas = await this.coursesService.loadAllCourses();
+            this.#courses.set(coursas.sort(sortCoursesBySeqNo));
+        } catch(err) {
+            alert('Error loading courses...!')
+            console.error(err);
+        }
+    }
 }
